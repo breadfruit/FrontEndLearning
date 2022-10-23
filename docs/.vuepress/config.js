0d52b1fea0b1555fa9e7397a3eaa01da09ec63eb
breadfruit <1845252982@qq.com>
2022-10-23 21:54:02 +0800

    week22

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,123 +1,127 @@
 const fs = require('fs')
 const path = require('path');
 module.exports = {
-    title: 'Study Of Toast',
-    description: '存量变增量',
-    theme: 'reco',
-    base: '/FrontEndLearning/',
-    mode: 'dark', // 默认 auto，auto 跟随系统，dark 暗色模式，light 亮色模式
-    modePicker: false, // 默认 true，false 不显示模式调节按钮，true 则显示
-    head: [
-      ['link', { rel: 'icon', href: '/logo.png' }],
-      ['link', { rel: 'manifest', href: '/manifest.json' }],
-      ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+  title: 'Study Of Toast',
+  description: '存量变增量',
+  theme: 'reco',
+  base: '/FrontEndLearning/',
+  mode: 'dark', // 默认 auto，auto 跟随系统，dark 暗色模式，light 亮色模式
+  modePicker: false, // 默认 true，false 不显示模式调节按钮，true 则显示
+  head: [
+    ['link', { rel: 'icon', href: '/logo.png' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+  ],
+  themeConfig: {
+    noFoundPageByTencent: false,
+    nav: [
+      { text: '首页', link: '/' },
+      {
+        text: 'Toast的前端学习笔记',
+        items: [
+          { text: 'Github', link: 'https://github.com/breadfruit' },
+          { text: '掘金', link: 'https://juejin.cn/user/1574156383563127' },
+          { text: '邮箱', link: '1845252982@qq.com' }
+        ]
+      }
     ],
-    themeConfig: {
-        noFoundPageByTencent: false,
-        nav: [
-            { text: '首页', link: '/' },
-            { 
-                text: 'Toast的前端学习笔记', 
-                items: [
-                    { text: 'Github', link: 'https://github.com/breadfruit' },
-                    { text: '掘金', link: 'https://juejin.cn/user/1574156383563127' },
-                    { text: '邮箱', link: '1845252982@qq.com' }
-                ]
-            }
+    sidebar: [
+      {
+        title: '欢迎学习',
+        path: '/',
+        collapsable: false, // 不折叠
+        children: [
+          { title: "学前必读", path: "/" }
+        ]
+      },
+      {
+        title: "前端基础",
+        path: '/handbook/Basic/index',
+        sidebarDepth: 1,
+        collapsable: false, // 不折叠
+        children: [
+          {
+            title: "CSS", sidebarDepth: 2, children: [
+              {
+                title: 'MinAndMax',
+                path: '/handbook/Basic/CSS/MinAndMax'
+              }, {
+                title: 'Percent',
+                path: '/handbook/Basic/CSS/Percent'
+              }
+            ]
+          },
+          {
+            title: "JavaScript", sidebarDepth: 2, children: [
+              {
+                title: 'JavaScript',
+                path: '/handbook/Basic/JavaScript/JavaScript'
+              }
+            ]
+          },
+          {
+            title: "TypeScript", sidebarDepth: 2, children: [
+              {
+                title: 'TypeScript',
+                path: '/handbook/Basic/TypeScript/TypeScript'
+              }
+            ]
+          },
         ],
-        sidebar: [
-            {
-                title: '欢迎学习',
-                path: '/',
-                collapsable: false, // 不折叠
-                children: [
-                    { title: "学前必读", path: "/" }
-                ]
-            },
-            {
-              title: "前端基础",
-              path: '/handbook/Basic/index',
-              sidebarDepth: 1, 
-              collapsable: false, // 不折叠
-              children: [
-                {
-                  title: "CSS", sidebarDepth: 2, children: [
-                    {
-                      title: 'MinAndMax',
-                      path: '/handbook/Basic/CSS/MinAndMax'
-                    },  {
-                      title: 'Percent',
-                      path: '/handbook/Basic/CSS/Percent'
-                    }
-                  ] 
-                },
-                {
-                  title: "JavaScript", sidebarDepth: 2, children: [
-                    {
-                      title: 'JavaScript',
-                      path: '/handbook/Basic/JavaScript/JavaScript'
-                    }
-                  ]
-                },
-                {
-                  title: "TypeScript", sidebarDepth: 2, children: [
-                    {
-                      title: 'TypeScript',
-                      path: '/handbook/Basic/TypeScript/TypeScript'
-                    }
-                  ]
-                },
-              ],
-            },
-            {
-              title: "前端工程化",
-              path: '/handbook/Project/index',
-              sidebarDepth: 1,   
-              collapsable: false, // 不折叠
-              children: [
-                { title: "框架", sidebarDepth: 2, path: "/handbook/Project/React" },
-                { title: "构建工具", sidebarDepth: 2, path: "/handbook/Project/Build" },
-                { title: "包管理工具", sidebarDepth: 2,path: "/handbook/Project/Npm" }
-              ],
-            },
-            {
-                  title: "MoreAWeek",
-                  path: '/moreAWeek',
-                  collapsable: false, // 不折叠
-                  children: [
-                    { title: "第一期", path: "/moreAWeek/week1" },
-                    { title: "第二期", path: "/moreAWeek/week2" },
-                    { title: "第三期", path: "/moreAWeek/week3" },
-                    { title: "第四期", path: "/moreAWeek/week4" },
-                    { title: "第五期", path: "/moreAWeek/week5" },
-                    { title: "第六期", path: "/moreAWeek/week6" },
-                    { title: "第七期", path: "/moreAWeek/week7" },
-                    { title: "第八期", path: "/moreAWeek/week8" },
-                    { title: "第九期", path: "/moreAWeek/week9" },
-                    { title: "第十期", path: "/moreAWeek/week10" },
-                    { title: "第十一期", path: "/moreAWeek/week11" },
-                    { title: "第十二期", path: "/moreAWeek/week12" },
-                    { title: "第十三期", path: "/moreAWeek/week13" },
-                    { title: "第十四期", path: "/moreAWeek/week14" },
-                    { title: "第十五期", path: "/moreAWeek/week15" },
-                    { title: "第十六期", path: "/moreAWeek/week16" },
-                    { title: "第十七期", path: "/moreAWeek/week17" },
-                    { title: "第十八期", path: "/moreAWeek/week18" }
-                  ],
-            }
+      },
+      {
+        title: "前端工程化",
+        path: '/handbook/Project/index',
+        sidebarDepth: 1,
+        collapsable: false, // 不折叠
+        children: [
+          { title: "框架", sidebarDepth: 2, path: "/handbook/Project/React" },
+          { title: "构建工具", sidebarDepth: 2, path: "/handbook/Project/Build" },
+          { title: "包管理工具", sidebarDepth: 2, path: "/handbook/Project/Npm" }
         ],
-        subSidebar: 'auto',
-        lastUpdated: '上次更新'
-    },
-    locales: {
-        '/': {
-          lang: 'zh-CN'
-        }
-    },
-    devServer: {
-      https: true,
-      key: fs.readFileSync(path.resolve(__dirname, './localhost+1-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, './localhost+1.pem'))
+      },
+      {
+        title: "MoreAWeek",
+        path: '/moreAWeek',
+        collapsable: false, // 不折叠
+        children: [
+          { title: "第一期", path: "/moreAWeek/week1" },
+          { title: "第二期", path: "/moreAWeek/week2" },
+          { title: "第三期", path: "/moreAWeek/week3" },
+          { title: "第四期", path: "/moreAWeek/week4" },
+          { title: "第五期", path: "/moreAWeek/week5" },
+          { title: "第六期", path: "/moreAWeek/week6" },
+          { title: "第七期", path: "/moreAWeek/week7" },
+          { title: "第八期", path: "/moreAWeek/week8" },
+          { title: "第九期", path: "/moreAWeek/week9" },
+          { title: "第十期", path: "/moreAWeek/week10" },
+          { title: "第十一期", path: "/moreAWeek/week11" },
+          { title: "第十二期", path: "/moreAWeek/week12" },
+          { title: "第十三期", path: "/moreAWeek/week13" },
+          { title: "第十四期", path: "/moreAWeek/week14" },
+          { title: "第十五期", path: "/moreAWeek/week15" },
+          { title: "第十六期", path: "/moreAWeek/week16" },
+          { title: "第十七期", path: "/moreAWeek/week17" },
+          { title: "第十八期", path: "/moreAWeek/week18" },
+          { title: "第十九期", path: "/moreAWeek/week19" },
+          { title: "第二十期", path: "/moreAWeek/week20" },
+          { title: "第二十一期", path: "/moreAWeek/week21" },
+          { title: "第二十二期", path: "/moreAWeek/week22" },
+        ],
+      }
+    ],
+    subSidebar: 'auto',
+    lastUpdated: '上次更新'
+  },
+  locales: {
+    '/': {
+      lang: 'zh-CN'
+    }
+  },
+  devServer: {
+    https: true,
+    key: fs.readFileSync(path.resolve(__dirname, './localhost+1-key.pem')),
+    cert: fs.readFileSync(path.resolve(__dirname, './localhost+1.pem'))
   },
   plugins: [
     // [
---
+++ docs/moreAWeek/week22.md
@@ -0,0 +1,55 @@
+---
+title: 第二十二期 AART打卡计划
+author: toast
+---
+
+# Algorithm
+
+#### [75. 颜色分类](https://leetcode.cn/problems/sort-colors/)
+
+其中比较新颖的地方是用两个指针进行遍历，对三个值进行排序。其中需要考虑中间值。
+
+#### [剑指 Offer 51. 数组中的逆序对](https://leetcode.cn/problems/shu-zu-zhong-de-ni-xu-dui-lcof/)
+
+利用在归并排序中左边的值大于右边值进行累计逆序值，其中因为归并排序中在归的过程中数组是排序好(升序)的，所以在计算逆序值的时候可以把自己右边所有的数值也加上去。
+
+# Review
+
+[如何编写 CommonJS 模块，以便它们的导出可以从 ES 模块实现按照名称导入](https://2ality.com/2022/10/commonjs-named-exports.html)
+
+在阅读文章之前复习一下JS模块基础
+
+>commonjs： 使用 `require` 来导入依赖，`exports` 来导出接口。
+>
+>ESM： `import` 声明依赖，`export` 声明接口。
+
+Node14.13.0以后，Node [添加了对 CJS named export 的支持](https://link.juejin.cn/?target=https%3A%2F%2Fnodejs.org%2Fen%2Fblog%2Frelease%2Fv14.13.0%2F)。其中使用了 [cjs-module-lexer](https://link.juejin.cn?target=https%3A%2F%2Fgithub.com%2Fguybedford%2Fcjs-module-lexer) 的语法分析库，对 CJS 模块内容进行静态语法分析，只支持简单的 `exports` 写法，如 `exports.name = ...` 或 `module.exports = require('...')`
+
+Consider the following ESM module:
+
+```js
+// main.mjs
+import {namedExport} from './lib.cjs'; // (A)
+console.log(namedExport); // 'yes'
+```
+
+The import in line A works if the CommonJS module looks like this:
+
+```js
+// lib.cjs
+exports.namedExport = 'yes';
+// module.exports.namedExport.
+```
+
+需要注意的是：
+
+1. cjs导入不是同步的。
+2. cjs不可以export对象
+
+# Tips
+
+[在Nodejs中使用Shell](https://www.npmjs.com/package/shelljs)
+
+shelljs中集成了shell的一些命令
+
+运用场景： 在npm script中可以使用node文件，实现一些比较复杂的工作流。
